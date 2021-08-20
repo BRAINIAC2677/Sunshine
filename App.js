180a8a15bc4d5ecc24baea1feaf87091daa5848e
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2021-08-20 22:48:02 +0600

    feat(solar-stat): inital structure was created.

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {color} from './styles/global';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={[color.primary, styles.container]}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ styles/global.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const color = StyleSheet.create({
+    primary:{
+        backgroundColor: "#eeeeee"
+    },
+    secondary:{
+        backgroundColor: "#222222"
+    },
+    "accent-1":{
+        backgroundColor: "red"
+    },
+    "accent-2":{
+        backgroundColor: "orange"
+    }
+});
+
+export {color};
