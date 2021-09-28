787720482fcc0a321ad9e235eeb7d76ea6e90713
Mohaiminul Islam <cadetmohaimin1999@gmail.com>
2021-09-28 22:21:37 +0600

    feat(calc): visualizer first page

---
+++ pages/calc/SolarData.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SolarData = () => {
+  return (
+    <View>
+      <Text>Solar data</Text>
+    </View>
+  );
+};
+
+export default SolarData;
+
+const styles = StyleSheet.create({});
--- pages/calc/index.js
+++ pages/calc/index.js
@@ -1,18 +1,124 @@
+import { Feather } from "@expo/vector-icons";
 import React from "react";
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import tw from "tailwind-react-native-classnames";
+import { fonts } from "../../styles/global";
 
-import { View, Text, TouchableOpacity } from "react-native";
-import { colors } from "../../styles/global";
+export default function Calc({ navigation }) {
+  const renderCard = (item) => (
+    <TouchableOpacity
+      style={tw.style("shadow-md", {
+        backgroundColor: item.bgColor,
+        ...styles.card,
+      })}
+    >
+      <View
+        style={tw.style("shadow", {
+          backgroundColor: item.secColor,
+          ...styles.iconBg,
+        })}
+      >
+        <Feather name="sun" size={24} color="white" />
+      </View>
+      <Text style={styles.titleText}>{item.title}</Text>
+    </TouchableOpacity>
+  );
 
-
-export default function Calc({navigation}) {
   return (
-    <View>
-      <Text>
-        Calc
-      </Text>
-      <TouchableOpacity style={{backgroundColor: colors.green, padding: 4}} onPress={() => navigation.navigate('Map')}>
-        <Text>Go to map</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Visualize Data</Text>
+
+      <ScrollView contentContainerStyle={{justifyContent: 'center', alignItems: 'center'}}>
+        <FlatList
+          data={data}
+          renderItem={({ item }) => renderCard(item)}
+          keyExtractor={(item) => item.id}
+          contentContainerStyle={{ alignSelf: "flex-start" }}
+          numColumns={Math.ceil(data.length / 2)}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+        />
+
+        <TouchableOpacity
+          style={tw`bg-gray-300 mt-8 w-48 justify-center items-center h-12 rounded-lg shadow`}
+          onPress={() => navigation.navigate("Map")}
+        >
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Feather name="map" size={16} color="black" />
+            <Text style={styles.buttonText}>Change Location</Text>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    marginTop: 50,
+  },
+  heading: {
+    marginBottom: 20,
+    fontFamily: fonts.bold,
+    fontSize: 24,
+  },
+  card: {
+    width: 150,
+    height: 150,
+    borderRadius: 20,
+    margin: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconBg: {
+    padding: 10,
+    borderRadius: 80,
+    padding: 15,
+    marginHorizontal: "auto",
+    marginBottom: 20,
+  },
+  titleText: {
+    fontFamily: fonts.bold,
+    fontSize: 20,
+    color: "white",
+  },
+  buttonText: {
+    marginLeft: 12,
+    fontFamily: fonts.semibold,
+  },
+});
+
+const data = [
+  {
+    id: 1,
+    title: "Solar Data",
+    screen: "",
+    bgColor: "#FF6968",
+    secColor: "#FF8280",
+  },
+  {
+    id: 2,
+    title: "Temperature",
+    screen: "",
+    bgColor: "#7A54FF",
+    secColor: "#946BFF",
+  },
+  {
+    id: 3,
+    title: "Wind Data",
+    screen: "",
+    bgColor: "#2AC3FF",
+    secColor: "#39D4FD",
+  },
+  {
+    id: 4,
+    title: "Humidity? Data",
+    screen: "",
+    bgColor: "#FF8F61",
+    secColor: "#FFA57A",
+  },
+];
