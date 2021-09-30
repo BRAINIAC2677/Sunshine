c00f788fd15cda3ac3f198eef50de5d979e7ded9
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2021-09-30 12:31:40 +0600

    feat(Home): primary home structure

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, ImageBackground, StyleSheet } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 export default function Home() {
   return (
-    <View>
-      <Text>
-        Home
-      </Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../../assets/home.jpg")}
+        resizeMode="cover"
+        style={tw.style(styles.bg, "justify-around items-center")}
+      >
+        <Text style={tw.style("text-white", "text-4xl", "capitalize")}>
+          welcome to sunshine
+        </Text>
+        <Text style={tw.style("text-white", "text-2xl", "capitalize")}>
+          make yourself solar efficient
+        </Text>
+      </ImageBackground>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bg: {
+    flex: 1,
+  },
+});
