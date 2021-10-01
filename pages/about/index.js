ae529e54c47f52884db4d6888423ba6f1a33f53c
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2021-10-01 23:24:32 +0600

    feat(solar calculator): calculator ui + graph changed

--- pages/about/index.js
+++ pages/about/index.js
@@ -9,7 +9,8 @@ import {
 } from "react-native";
 import { TextField } from "@ubaids/react-native-material-textfield";
 import tw from "tailwind-react-native-classnames";
-import { ProgressChart } from "react-native-chart-kit";
+import { BarChart } from "react-native-chart-kit";
+import { colors, fonts } from "../../styles/global";
 
 export default function About() {
   let refConsumption = React.createRef();
@@ -22,12 +23,17 @@ export default function About() {
   const [showChart, setShowChart] = useState(false);
 
   const panelData = {
-    labels: [
-      `${Math.ceil(noOfPanels / 4)} panels`,
-      `${Math.ceil(noOfPanels / 2)} panels`,
-      `${noOfPanels} panels`,
+    labels: ["25%", "50%", "75%", "100%"],
+    datasets: [
+      {
+        data: [
+          Math.ceil(noOfPanels / 4),
+          Math.ceil(noOfPanels / 2),
+          Math.ceil((3 * noOfPanels) / 4),
+          noOfPanels,
+        ],
+      },
     ],
-    data: [0.25, 0.5, 1],
   };
 
   const calculate = () => {
@@ -43,54 +49,69 @@ export default function About() {
   };
 
   const chartConfig = {
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    backgroundGradientFrom: colors.secondaryBg,
+    backgroundGradientTo: colors.secondaryBg,
+    color: (opacity = 1) => `rgba(248, 198, 28, ${opacity})`,
   };
 
   return (
-    <View style={tw.style(styles.container, "justify-around bg-gray-800 p-4")}>
-      <Text style={tw.style("text-4xl text-center text-white")}>
+    <View
+      style={tw.style(styles.container, "p-4", {
+        backgroundColor: colors.primaryBg,
+      })}
+    >
+      <Text
+        style={tw.style(
+          "pt-20 capitalize tracking-wide text-4xl text-center text-white",
+          { color: colors.accent, fontFamily: fonts.bold }
+        )}
+      >
         solar calculator
       </Text>
-      <View>
+      <View style={tw.style("pt-10")}>
         <TextField
           label="Monthly Power Consumption(kw-hr)"
+          labelTextStyle={{ fontFamily: fonts.regular }}
+          inputTextStyle={{ fontFamily: fonts.regular }}
           keyboardType="phone-pad"
           onChangeText={setConsumption}
           onFocus={() => setShowChart(false)}
           value={consumption}
           textColor="white"
           baseColor="white"
-          tintColor="white"
+          tintColor={colors.accent}
           fontSize={20}
           labelFontSize={15}
           lineWidth={2}
           ref={refConsumption}
-          style={tw.style(styles.input)}
         ></TextField>
         <TextField
-          label="Panel Wattage"
+          label="Solar Panel Wattage(watt)"
+          labelTextStyle={{ fontFamily: fonts.regular }}
+          inputTextStyle={{ fontFamily: fonts.regular }}
           keyboardType="phone-pad"
           onChangeText={setWattage}
           onFocus={() => setShowChart(false)}
           value={wattage}
           textColor="white"
           baseColor="white"
-          tintColor="white"
+          tintColor={colors.accent}
           fontSize={20}
           labelFontSize={15}
           lineWidth={2}
           ref={refWattage}
-          style={tw.style(styles.input)}
         ></TextField>
         <TouchableOpacity
-          style={tw.style("self-center my-8 p-2 w-48 rounded-md bg-gray-500")}
+          style={tw.style("self-center my-8 p-2 w-48 rounded-md", {
+            backgroundColor: colors.accent,
+          })}
           onPress={() => calculate()}
         >
           <View>
             <Text
-              style={tw.style(
-                "capitalize text-center text-lg text-white font-bold"
-              )}
+              style={tw.style("capitalize text-center text-lg text-black", {
+                fontFamily: fonts.semibold,
+              })}
             >
               calculate
             </Text>
@@ -98,16 +119,27 @@ export default function About() {
         </TouchableOpacity>
       </View>
       {showChart && (
-        <ProgressChart
-          data={panelData}
-          width={Dimensions.get("window").width - 60}
-          height={200}
-          strokeWidth={15}
-          radius={30}
-          chartConfig={chartConfig}
-          hideLegend={false}
-          style={tw.style("self-center rounded-md")}
-        ></ProgressChart>
+        <View>
+          <BarChart
+            data={panelData}
+            width={Dimensions.get("window").width - 30}
+            height={220}
+            chartConfig={chartConfig}
+            yAxisSuffix=" Panels "
+            horizontalLabelRotation={30}
+            fromZero={true}
+            showValuesOnTopOfBars={true}
+            style={tw.style("self-center", { borderRadius: 10 })}
+          />
+          <Text
+            style={tw.style("mt-2 capitalize text-white text-sm", {
+              fontFamily: fonts.regular,
+            })}
+          >
+            no of solar panels vs percentage of generated energy of your total
+            consumption
+          </Text>
+        </View>
       )}
     </View>
   );
