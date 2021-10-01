9907de542ed6b0ddf8cba0fee2562c625a84b404
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2021-10-01 20:09:08 +0600

    feat(home): home ui done

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -1,22 +1,52 @@
 import React from "react";
-import { Text, View, ImageBackground, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import { AntDesign } from "@expo/vector-icons";
+import { colors } from "../../styles/global";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={require("../../assets/home.jpg")}
-        resizeMode="cover"
-        style={tw.style(styles.bg, "justify-around items-center")}
+    <View style={tw.style(styles.container)}>
+      <View
+        style={tw.style(
+          styles.upper,
+          "justify-center items-center rounded-b-full"
+        )}
       >
-        <Text style={tw.style("text-white", "text-4xl", "capitalize")}>
-          welcome to sunshine
+        <Text
+          style={tw.style(
+            `capitalize tracking-widest text-4xl text-black text-center`
+          )}
+        >
+          sunshine{"\n\n"}
+          <Text style={tw.style(`tracking-wide text-xl`)}>
+            {" "}
+            make yourself solar efficient
+          </Text>
         </Text>
-        <Text style={tw.style("text-white", "text-2xl", "capitalize")}>
-          make yourself solar efficient
+        <Image
+          source={require("../../assets/sun.png")}
+          style={tw.style("drop-shadow-lg")}
+        />
+      </View>
+      <View style={tw.style(styles.lower, "px-6 justify-around")}>
+        <Text
+          style={tw.style(styles.tag, "mt-4 capitalize text-left text-3xl")}
+        >
+          explore your solar {"\n"}possibilities
         </Text>
-      </ImageBackground>
+        <TouchableOpacity
+          style={tw.style(
+            styles.button,
+            "flex-row justify-center items-center h-12 p-4 rounded-xl"
+          )}
+        >
+          <Text style={tw.style("mr-4 capitalize font-bold text-xl")}>
+            get started
+          </Text>
+          <AntDesign name="arrowright" size={24} color="black" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -24,8 +54,19 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.primaryBg,
   },
-  bg: {
-    flex: 1,
+  upper: {
+    flex: 4,
+    backgroundColor: colors.accent,
+  },
+  lower: {
+    flex: 2,
+  },
+  tag: {
+    color: colors.accent,
+  },
+  button: {
+    backgroundColor: colors.accent,
   },
 });
--- styles/global.js
+++ styles/global.js
@@ -1,28 +1,32 @@
 export const colors = {
-    primary: "#F8C61C",
-    bg1: "#252729",
-    bg2: "#2B3236",
-    secondary: "#554099",
-    primaryButton: "#667FFD",
+  primary: "#F8C61C",
+  bg1: "#252729",
+  bg2: "#2B3236",
+  secondary: "#554099",
+  primaryButton: "#667FFD",
 
-    text1: "#fcfcfc",
-    text2: "#6C757D",
-    text3: "#a9bbcc",
+  primaryBg: "#252729",
+  secondaryBg: "#2B3236",
+  accent: "#F8C61C",
 
-    lightbg: "#FCFCFC",
-    darkbg: "#161625",
-    redbg: "#FFE0E6",
-    bluebg: "#DFEEFF",
-    greenbg: "#E4F4E8",
-    graybg: "#EDEEEF",
-    red: "#FF073A",
-    green: "#28A745",
-    blue: "#007BFF",
-    gray: "#6C757D",
-}
+  text1: "#fcfcfc",
+  text2: "#6C757D",
+  text3: "#a9bbcc",
+
+  lightbg: "#FCFCFC",
+  darkbg: "#161625",
+  redbg: "#FFE0E6",
+  bluebg: "#DFEEFF",
+  greenbg: "#E4F4E8",
+  graybg: "#EDEEEF",
+  red: "#FF073A",
+  green: "#28A745",
+  blue: "#007BFF",
+  gray: "#6C757D",
+};
 
 export const fonts = {
-    bold: 'barlow-bold',
-    regular: 'barlow-regular',
-    semibold: 'barlow-semibold',
-}
+  bold: "barlow-bold",
+  regular: "barlow-regular",
+  semibold: "barlow-semibold",
+};
